1f0f4b2ed61f02b68da02dd1c9afc4807a6bc4a7
Joseph Garcia <Joseph_Garcia@ucne.edu.do>
2024-01-31 15:58:50 -0400

    first commit

---
+++ CRUD with WF/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUD_with_WF.model;
+
+namespace CRUD_with_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentacion.FrmTabla oFrmTabla = new Presentacion.FrmTabla();
+
+            oFrmTabla.ShowDialog();
+
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            using (CrudEntities db = new CrudEntities())
+            {
+                var data = from d in db.Table_datos
+                           select d;
+                dataGridView1.DataSource = data.ToList();
+            }
+        }
+
+        private int? GetId()
+        {
+            try
+            {
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+
+            if(id != null)
+            {
+                Presentacion.FrmTabla oFrmTable = new Presentacion.FrmTabla(id);
+                oFrmTable.ShowDialog();
+
+                CargarDatos();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+
+            if (id != null)
+            {
+                using (CrudEntities db = new CrudEntities())
+                {
+                    Table_datos oTabla = db.Table_datos.Find(id);
+                    db.Table_datos.Remove(oTabla);
+
+                    db.SaveChanges();
+                }
+                CargarDatos();
+            }
+        }
+    }
+}
---
+++ CRUD with WF/Presentacion/FrmTabla.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUD_with_WF.model;
+
+namespace CRUD_with_WF.Presentacion
+{
+    public partial class FrmTabla : Form
+    {
+        public int? id;
+        Table_datos otabla = null;
+        public FrmTabla(int? id=null)
+        {
+            InitializeComponent();
+
+            this.id = id;
+            if(id != null)
+            {
+                CargarDatos();
+            }
+        }
+
+        private void CargarDatos()
+        {
+            using (CrudEntities db = new CrudEntities())
+            {
+                otabla = db.Table_datos.Find(id);
+                txtNombre.Text = otabla.nombre;
+                txtCorreo.Text = otabla.correo;
+                txtFechaNacimiento.Text = otabla.fecha_nacimiento;
+
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (CrudEntities db = new CrudEntities())
+            {
+                if(id == null)
+                {
+                    otabla = new Table_datos();
+                }
+
+                otabla.nombre = txtNombre.Text;
+                otabla.correo = txtCorreo.Text;
+                otabla.fecha_nacimiento = txtFechaNacimiento.Text;
+
+                if(id == null)
+                {
+                    db.Table_datos.Add(otabla);
+                }
+                else
+                {
+                    db.Entry(otabla).State = System.Data.Entity.EntityState.Modified;
+                }
+
+                db.SaveChanges();
+                this.Close();
+
+            }
+        }
+
+        private void FrmTabla_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
